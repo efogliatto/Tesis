084a44ce50e2611616b6d163f68bbcc4a0fd2f93
Ezequiel Fogliatto (CAB) <fogliatto@cab.cnea.gov.ar>
2021-01-14 10:33:40 -0300

    Incorporaci\'on de los resultados de simulaci\'on a Bo_vs_Ja

--- Imagenes/FC72/Bo_vs_Ja/Bo_vs_Ja.py
+++ Imagenes/FC72/Bo_vs_Ja/Bo_vs_Ja.py
@@ -28,21 +28,52 @@ if __name__ == "__main__":
 
     hutter = np.loadtxt('D_vs_supheat.dat', unpack=True)
 
-    rho_l = 1620
+    rho_l_exp = 1620.
 
-    g = 9.81
+    g_exp = 9.81
 
-    sigma_s = 0.00827
+    sigma_s_exp = 0.00827
 
-    cv = 1098
+    cv_exp = 1098.
 
-    hfg = 84500  
+    hfg_exp = 84500.  
 
-    # plt.errorbar(hutter[0], hutter[1]/1000, yerr=hutter[2]/1000, marker='o', mfc='k', fmt='o', ecolor='k', label='Exp')
+
+    Ja_exp = [ t*cv_exp/hfg_exp for t in hutter[0] ]
+
+    Bo_exp = [ (rho_l_exp*g_exp/sigma_s_exp)*((d/1000)**2) for d in hutter[1] ]
+
+    Bo_exp_err = [ 2*(rho_l_exp*g_exp/sigma_s_exp)*(d/1000)*(e/1000) for d,e in zip(hutter[1],hutter[2]) ]
+
     
-    plt.errorbar([t*cv/hfg for t in hutter[0]], [rho_l*g*(d/1000)*(d/1000)/sigma_s for d in hutter[1]], yerr=[rho_l*g*(d/1000)*(d/1000)/(sigma_s) for d in hutter[2]], marker='o', mfc='k', fmt='o', ecolor='k', label='Exp')
+    # plt.errorbar(hutter[0], hutter[1], yerr=hutter[2], marker='o', mfc='k', fmt='o', ecolor='k', label='Experimento')
+    plt.errorbar(Ja_exp, Bo_exp, yerr=Bo_exp_err, marker='o', mfc='k', fmt='o', ecolor='k', label='Experimento')
 
 
+
+
+
+    # Simulaciones
+
+    lb = np.loadtxt('D_vs_supheat_LB.dat', unpack=True)
+
+    rho_l_lb = 7.949
+
+    sigma_s_lb = 0.26874*2.
+
+    cv_lb = 11.
+
+    hfg_lb = 0.315574
+
+    Ts_lb = 0.0262
+
+
+    Ja_lb = [ (t-Ts_lb)*cv_lb/hfg_lb for t in lb[0] ]
+
+    Bo_lb = [ (rho_l_lb*g/sigma_s_lb)*(d**2) for d,g in zip(lb[1],lb[2]) ]
+    
+    # plt.plot(Ja_lb, Bo_lb, marker='s', mfc='r', mec='r', linestyle='None', label='LB')
+    plt.errorbar(Ja_lb, Bo_lb, marker='s', mfc='r', mec='r', fmt='s', ecolor='r', label='LB')
 
 
 
@@ -57,7 +88,7 @@ if __name__ == "__main__":
 
     plt.xlabel('Ja')
 
-    plt.legend(loc = 'best', framealpha=1)
+    plt.legend(loc = 'upper left', framealpha=1)
 
 
 
